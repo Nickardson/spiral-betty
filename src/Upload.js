575d58fd045a26211ab3e42f509c798b0c76057a
shalanah <shalanahfaith@gmail.com>
2018-06-02 20:55:28 -0500

    Closes #30 - Add drag-drop capabilities for uploading img

--- src/Upload.js
+++ src/Upload.js
@@ -9,18 +9,61 @@ const Label = styled.label`
   transition: .2s;
   border-radius: 100%;
   cursor: pointer;
-  background-color: #efefef;
 `
 
 // TODO: animation timing, store as var
 class Upload extends Component {
-  state = {hover: false}
+  state = {hover: false, dragging: 0}
+  onDrop = (e) => {
+    e.preventDefault()
+    // TODO: set state
+    this.props.onChange({target: e.dataTransfer})
+    if (this.props.isFirefox) return // FF kinda sucks at this stuff
+    this.setState(({dragging: d, ...props}) => {return {...props, dragging: 0}}, () => {
+      // Keeping children from firing (pointer events turned off)
+      document.getElementsByTagName( 'html' )[0].classList.remove('file-dragging')
+    })
+  }
+  onDragEnter = (e) => {
+    e.preventDefault()
+    if (this.props.isFirefox) return // FF kinda sucks at this stuff
+    document.getElementsByTagName('html')[0].classList.add('file-dragging')
+    this.setState(({dragging: d, ...props}) => {return {...props, dragging: d + 1}})
+  }
+  onDragOver = (e) => {e.preventDefault()}
+  onDragLeave = (e) => {
+    e.preventDefault()
+    if (this.props.isFirefox) return // FF kinda sucks at this stuff
+    this.setState(({dragging: d, ...props}) => {return {...props, dragging: d - 1}}, () => {
+      if (this.state.dragging <= 0) {
+         // Turn back on pointer events
+        document.getElementsByTagName( 'html' )[0].classList.remove('file-dragging')
+      }
+    })
+  }
   onMouseEnter = () => { this.setState({hover: true}) }
   onMouseLeave = () => { this.setState({hover: false}) }
+  componentDidMount () {
+    window.addEventListener('dragenter', this.onDragEnter)
+    window.addEventListener('dragleave', this.onDragLeave)
+    window.addEventListener('dragover', this.onDragOver)
+    window.addEventListener('drop', this.onDrop)
+  }
+  componentWillUnmount () {
+    window.removeEventListener('dragenter', this.onDragEnter)
+    window.removeEventListener('dragleave', this.onDragLeave)
+    window.removeEventListener('dragover', this.onDragOver)
+    window.removeEventListener('drop', this.onDrop)
+  }
   render () {
     const {onChange, blobUrl} = this.props
-    const active = !blobUrl
-    const {hover} = this.state
+    const {hover, dragging} = this.state
+    const active = !blobUrl || dragging
+    const showHover = dragging || hover
+    const line = dragging ? 'dashed' : 'solid'
+    let backgroundColor = '#efefef'
+    if (showHover) backgroundColor = '#fff'
+    if (blobUrl && dragging) backgroundColor = 'rgba(255,255,255,.8)'
     return (
       <Label
         className='pos-full'
@@ -29,19 +72,24 @@ class Upload extends Component {
         style={{
           pointerEvents: active ? '' : 'none',
           opacity: active ? 1 : 0,
-          border: hover ? '3px solid var(--accent)' : '1px solid rgba(0,0,0,.25)',
+          backgroundColor,
+          border: showHover ? `3px ${line} var(--accent)` : `1px ${line} rgba(0,0,0,.25)`,
         }}>
-        <IconPhotoPlaceholder length={100} className="pos-center" active={hover} />
+        <div className='pos-full' style={{backgroundColor: 'var(--accent)', transition: '.2s', borderRadius: '100%', opacity: showHover ? 0.2 : 0}} />
+        <IconPhotoPlaceholder
+          length={100}
+          className="pos-center"
+          active={showHover} />
         <div
           className='pos-center'
           style={{marginTop: 68}}>
           <UploadText
             style={{
               textAlign: 'center',
-              color: hover ? 'var(--accent)' : '',
+              color: showHover ? 'var(--accent)' : '',
               transition: '.2s'
             }}>
-            + Upload image
+            {blobUrl ? '+ Replace image' : '+ Upload image'}
           </UploadText>
         </div>
         {/* Hide input so we can style label */}
@@ -60,8 +108,8 @@ class Upload extends Component {
 }
 
 const mapStateToProps = (state) => {
-  const {img: {blobUrl}} = state
-  return {blobUrl}
+  const {img: {blobUrl}, temp: {isFirefox}} = state
+  return {blobUrl, isFirefox}
 }
 export default connect(
   mapStateToProps
