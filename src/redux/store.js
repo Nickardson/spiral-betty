f65b3fb299895fc8b839309f8cb4ac5f08967e2f
shalanah <shalanahfaith@gmail.com>
2018-10-14 20:37:20 -0500

    Add redux dev tools to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,8 @@ const initialState = {}
 
 const store = createStore(
   rootReducer,
-  initialState
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 export default store
